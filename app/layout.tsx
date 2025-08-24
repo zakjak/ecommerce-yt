cfaa74aff750e206979df5e4b9b5ca471874813f
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2025-08-24 16:40:11 -0500

    implement initial styles

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Jost } from "next/font/google";
+import "./globals.css";
+
+const jost = Jost({
+  variable: "--font-jost",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Nike",
+  description: "An e-commerce platform for Nike shoes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${jost.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
